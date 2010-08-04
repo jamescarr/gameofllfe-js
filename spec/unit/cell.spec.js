ce194a472c4b77bac43c08feb046fe946637aeb0
James Carr <james.r.carr@gmail.com>
2010-08-03 23:25:01 -0500

    initial commit

--- spec/unit/cell.spec.js
+++ spec/unit/cell.spec.js
@@ -22,4 +22,89 @@ describe 'Evolving a dead cell'
     
     board.at(1,1).should.be '.'        
   end
+  
+  it 'Dead cell with 2 neighbor stays dead'
+    var board = createNewGame([
+      ['.','x','x'],
+      ['.','.','.'],
+      ['.','.','.']            
+    ])
+    
+    board.evolve()
+    
+    board.at(1,1).should.be '.'        
+  end
+  
+  it 'Dead cell with 3 neighbors comes to life'
+    var board = createNewGame([
+      ['x','x','x'],
+      ['.','.','.'],
+      ['.','.','.']            
+    ])
+    
+    board.evolve()
+    
+    board.at(1,1).should.be 'x'        
+  end
+  
+  it 'Dead cell with 4 neighbors remains dead'
+    var board = createNewGame([
+      ['x','x','x'],
+      ['x','.','.'],
+      ['.','.','.']            
+    ])
+    
+    board.evolve()
+    
+    board.at(1,1).should.be '.'        
+  end
+  
+  it 'should evolve dead cell in corner with 3 neighbors'
+    print('running dead cell in corner with 3 neighbors');
+    var board = createNewGame([
+      ['.','x','.'],
+      ['x','x','.'],
+      ['.','.','.']            
+    ])
+    
+    board.evolve()
+    
+    board.at(0,0).should.be 'x'        
+  end
+  
+  it 'Dead cell with 5 neighbors remain dead'
+    var board = createNewGame([
+      ['x','x','x'],
+      ['x','.','x'],
+      ['.','.','.']            
+    ])
+    
+    board.evolve()
+    
+    board.at(1,1).should.be '.'        
+  end
+  
+  it 'Dead cell with 6 neighbors remain dead'
+    var board = createNewGame([
+      ['x','x','x'],
+      ['x','.','x'],
+      ['.','.','x']            
+    ])
+    
+    board.evolve()
+    
+    board.at(1,1).should.be '.'        
+  end
+  
+  it 'Dead cell with 7 neighbors remain dead'
+    var board = createNewGame([
+      ['x','x','x'],
+      ['x','.','x'],
+      ['.','x','x']            
+    ])
+    
+    board.evolve()
+    
+    board.at(1,1).should.be '.'        
+  end
 end
